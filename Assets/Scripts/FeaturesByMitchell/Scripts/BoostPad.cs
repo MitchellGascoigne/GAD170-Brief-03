6a59b33a626ca59d938b9d87f348e505f093eb53
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2022-08-02 12:03:50 +1000

    Initial commit

---
+++ Assets/Scripts/FeaturesByMitchell/Scripts/BoostPad.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MitchellGascoigne
+{
+    public class BoostPad : MonoBehaviour 
+        // Copy from shellexplosion script
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            BoostPadCollision();
+        }
+        private void BoostPadCollision()
+        {
+            //When something collides with the boost pad we run this method
+            //Check if a tank has collided with the boost pad
+            //Check what angle the tank collided at
+            //If angle is between 0 and 90 degrees increase speed to 24
+            //Else decrease tanks speed to 6
+            //After 2 seconds, reset the tanks speed to base set of 12
+        }
+    }
+
+}
+
+// use events to make this happen (example in ShellExplosion.Boom script)
