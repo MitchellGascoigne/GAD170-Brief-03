a32be843bd9e55d13fa4d2eef6b97d76f32f8ab0
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2022-08-19 07:39:30 +1000

    Updated Events Manager

--- Assets/Scripts/FeaturesByMitchell/Scripts/Boost Pad Scripts/BoostPadEventsManager.cs
+++ Assets/Scripts/FeaturesByMitchell/Scripts/Boost Pad Scripts/BoostPadEventsManager.cs
@@ -1,57 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 
 namespace MitchellGascoigne
 {
     public class BoostPadEventsManager : MonoBehaviour
     {
-        // Use events (Events enable a class or object to notify other classes or objects when something of interest occurs) 
-        // Use the Event to to add data to a "Boost Pad Counter" when the Trigger Collider is activated 
-        // Unity can save and load those values (all public fields are serialized by default) even though they are not public. 
-        [SerializeField] private string playerTankTag = "player";
-        // Creates a delegate that refers to the method for BoostPadActivationAction()
-        public delegate void BoostPadActivationAction();
-        public static event BoostPadActivationAction OnBoostPadActivation;
-
-        // OnTriggerEnter is called when the Collider other enters the trigger (set the boostpad as the trigger)
-        private void OnTriggerEnter(Collider other)
-        { 
-            if (other.CompareTag("Player")) 
-            {
-                // If the tank collides with the trigger zone, double its speed
-                other.GetComponent<Tank>().tankMovement.speed = 24;
-                //Debug.Log(other.gameObject.name);
-                Debug.Log("You have used the Boost Pad");
-
-                // Check if there is an absence of data or simply no data
-                // If this is the case, call the OnBoostPadActivationMethod()
-                if (OnBoostPadActivation != null)
-                {
-                    OnBoostPadActivation();
-                }
-                // Call the SpeedTime() method
-                SpeedTime();
-
-                // The Speedtime() method that returns an IEnumerator interface
-                IEnumerator SpeedTime()
-                {
-                    // Have the boost pad effect wear off after 2 seconds
-                    yield return new WaitForSeconds(1f);
-                    // Have the speed set back to original speed of 12 after time
-                    other.GetComponent<Tank>().tankMovement.speed = 12;
-                }
-                    // StopCoroutine takes one of three arguments which specify which coroutine is stopped:
-                    // A string function naming the active coroutine
-                    // The IEnumerator variable used earlier to create the coroutine.
-                    // The Coroutine to stop the manually created Coroutine.
-                if (other.gameObject.CompareTag("Player"))
-                {
-                    StopCoroutine(SpeedTime());
-                    StartCoroutine(SpeedTime());
-                }
-            }
+        // For our Event system we need these things:
+        // Announce event at particular times or when a particular collision occurs, etc...
+        // We want to make sure our UI is LISTENING for the Event
+        // When the Event is announced, we want to update our UI
+        [SerializeField] private TextMeshProUGUI boostPadCounter;
+        [SerializeField] private int boostPadCount = 0;
+        private void OnEnable()
+        {
+            BoostPadCounter.OnBoostPadActivation += IncreaseBoostPadCount;
+        }   
+        private void OnDisable()
+        {
+            BoostPadCounter.OnBoostPadActivation -= IncreaseBoostPadCount;
+        }
+        // When the method is called, have the boost pad counter to add a value of "1"
+        // Include the string ""Boost Pads Used:" followed by the amount of times it has been used
+        private void IncreaseBoostPadCount()
+        {
+            boostPadCount++;
+            boostPadCounter.text = "Boost Pads Used: " + boostPadCount;
         }
     }
 }
